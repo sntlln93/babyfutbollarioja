4d580f20f93feff8260d13b6f695e0bb6b94677c
Matias Santillan <sntlln.93@gmail.com>
2021-04-25 07:47:25 -0300

    removed unused elements. still need to work on the notifications

--- app/Http/Controllers/Dashboard/HomeController.php
+++ app/Http/Controllers/Dashboard/HomeController.php
@@ -2,6 +2,10 @@
 
 namespace App\Http\Controllers\Dashboard;
 
+use App\Models\Game;
+use App\Models\Post;
+use App\Models\Event;
+use App\Models\Tournament;
 use App\Http\Controllers\Controller;
 
 class HomeController extends Controller
@@ -23,6 +27,15 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('dashboard.home');
+        $tournament_count = Tournament::all()->count();
+        $game_count = Game::all()->count();
+        $goal_count = Event::where('type', 'gol')->count();
+        $post_count = Post::all()->count();
+
+        return view('dashboard.home')
+            ->with('tournament_count', $tournament_count)
+            ->with('goal_count', $goal_count)
+            ->with('post_count', $post_count)
+            ->with('game_count', $game_count);
     }
 }
