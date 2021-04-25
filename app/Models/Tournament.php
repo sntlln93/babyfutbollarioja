b5bd811afd4836530a2d2de46ffb4decf3ea131e
Matias Santillan <sntlln.93@gmail.com>
2021-04-25 17:07:14 -0300

    Revert "Revert "added image to tournament.create and tournament@store""

--- app/Models/Tournament.php
+++ app/Models/Tournament.php
@@ -2,6 +2,8 @@
 
 namespace App\Models;
 
+use App\Models\Game;
+use App\Models\Category;
 use Illuminate\Database\Eloquent\Model;
 
 class Tournament extends Model
@@ -21,5 +23,10 @@ class Tournament extends Model
     public function type()
     {
         return $this->belongsTo(Type::class);
+    }
+
+    public function image()
+    {
+        return $this->morphOne(Image::class, 'imageable');
     }
 }
