8e7a1a0bc2f88f24688acb679c9d84cb56160444
Matias Santillan <sntlln.93@gmail.com>
2021-09-07 15:02:41 -0300

    🐛 Defaulting to something else when there is no tournament

--- app/Http/Controllers/Web/WebController.php
+++ app/Http/Controllers/Web/WebController.php
@@ -11,13 +11,15 @@ class WebController extends Controller
 {
     public function index()
     {
-        $tournament = Tournament::where('is_active', true)->first();
+        $tournaments = Tournament::all();
+        $tournament = $tournaments->where('is_active', true)->first();
         $posts = Post::orderBy('created_at', 'desc')->take(5)->get();
         $clubs = Club::all();
 
         return view('web.welcome')
             ->with('clubs', $clubs)
-            ->with('tournament', $tournament)
+            ->with('tournament', null)
+            ->with('tournaments', $tournaments)
             ->with('posts', $posts);
     }
 
--- resources/views/web/layouts/menu.blade.php
+++ resources/views/web/layouts/menu.blade.php
@@ -2,17 +2,19 @@
     <a href="{{ route('web.index') }}">Inicio</a>
 </li>
 
+@if($tournaments->count() > 0)
+
 <li class="current">
     <a href="{{ route('web.tournaments')}}">Torneos</a>
     <ul class="sub-current">
+        @foreach ($tournaments as $tournament)
         <li>
-            <a href="{{ route('web.tournament', ['tournament' => 1])}}">Regional</a>
+            <a href="{{ route('web.tournament', ['tournament' => $tournament->id])}}">{{ $tournament->name }}</a>
         </li>
-        <li>
-            <a href="{{ route('web.tournament', ['tournament' => 2])}}">Federal</a>
-        </li>
+        @endforeach
     </ul>
 </li>
+@endif
 
 <li>
     <a href="{{ route('web.sponsors') }}">Auspiciantes</a>
