6955e9fb5f4344471f292febc98d28db435839bf
Matias Santillan <sntlln.93@gmail.com>
2021-09-12 03:10:59 -0300

    🎨 Added fontawesome's solid icons

--- resources/views/web/layouts/meta.blade.php
+++ resources/views/web/layouts/meta.blade.php
@@ -1,9 +1,10 @@
 <!-- Basic -->
 <meta charset="utf-8">
 <title>ARBFI - Asociación Riojana de Baby Fútbol Infantil</title>
-<meta name="keywords" content="babyfutbol baby futbol arbfi larioja la rioja matiassantillan sntlln93 matias santillan" />
+<meta name="keywords"
+    content="babyfutbol baby futbol arbfi larioja la rioja matiassantillan sntlln93 matias santillan" />
 <meta name="description" content="ARBFI - Asociación Riojana de Baby Fútbol Infantil">
 <meta name="author" content="htpps://github.com/sntlln93">
-
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/solid.min.css">
 <!-- Mobile Metas -->
 <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
