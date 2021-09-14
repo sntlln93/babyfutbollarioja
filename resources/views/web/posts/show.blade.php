ffb385e31ba022605a8f6078632ac17357ba54f0
Matias Santillan <sntlln.93@gmail.com>
2021-09-14 04:29:14 -0300

    💄 Fixing padding on show post

--- resources/views/web/posts/show.blade.php
+++ resources/views/web/posts/show.blade.php
@@ -19,17 +19,20 @@
     <div class="white-section pt-5 mb-1">
         <div class="container">
             <div class="row no-line-height">
-                <div class="post__header">
-                    <h2>{{ $post->title }}</h2>
-                    <p>{{ $post->author->full_name }}, {{ $post->created_at->diffForHumans() }}</p>
-                </div>
-                <div class="post__body">
-                    @if($post->image)
-                    <img src="{{ asset('storage/'.$post->image->path) }}" alt="{{ $post->title }}" class="img-fluid">
-                    @endif
-                    <p>
-                        {{ $post->body }}
-                    </p>
+                <div class="col-sm-12">
+                    <div class="post__header">
+                        <h2>{{ $post->title }}</h2>
+                        <p>{{ $post->author->full_name }}, {{ $post->created_at->diffForHumans() }}</p>
+                    </div>
+                    <div class="post__body">
+                        @if($post->image)
+                        <img src="{{ asset('storage/'.$post->image->path) }}" alt="{{ $post->title }}"
+                            class="img-fluid">
+                        @endif
+                        <p>
+                            {{ $post->body }}
+                        </p>
+                    </div>
                 </div>
             </div>
         </div>
