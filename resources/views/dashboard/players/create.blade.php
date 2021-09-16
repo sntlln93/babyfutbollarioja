1184f42313289f5ae9ebe13599f87e0c8c790b22
Matias Santillan <sntlln.93@gmail.com>
2021-09-16 06:02:59 -0300

    🐛 Fixed bug with wrong endpoint

--- resources/views/dashboard/players/create.blade.php
+++ resources/views/dashboard/players/create.blade.php
@@ -1,151 +1,152 @@
 @extends('dashboard.layouts.app')
 
 @section('content')
-    <!-- Page Heading -->
-    <div class="d-sm-flex align-items-center justify-content-between mb-4">
-        <h1 class="h3 mb-0 text-gray-800">Nuevo jugador</h1>
-    </div>
+<!-- Page Heading -->
+<div class="d-sm-flex align-items-center justify-content-between mb-4">
+    <h1 class="h3 mb-0 text-gray-800">Nuevo jugador</h1>
+</div>
 
-    <div class="row">
-        <div class="col-sm-12 col-md-8 mx-auto">
-            <form action="{{ route('players.store') }}" method="POST" class="needs-validation" novalidate
-                enctype="multipart/form-data">
-                @csrf
+<div class="row">
+    <div class="col-sm-12 col-md-8 mx-auto">
+        <form action="{{ route('players.store') }}" method="POST" class="needs-validation" novalidate
+            enctype="multipart/form-data">
+            @csrf
 
-                <div class="form-row mb-2">
-                    <div class="col-sm-12">
-                        <label for="lastname">Apellido</label>
-                        <input type="text" class="form-control @error('lastname') border-danger @enderror" id="lastname"
-                            name="lastname" placeholder="Apellidos" value="{{ old('lastname') }}" required>
-                        <span class="invalid-feedback" role="alert">
-                            <strong>Este campo es obligatorio</strong>
-                        </span>
-                        @error('lastname')
-                            <span class="text-danger" role="alert">
-                                <strong>{{ $message }}</strong>
-                            </span>
-                        @enderror
-                    </div>
+            <div class="form-row mb-2">
+                <div class="col-sm-12">
+                    <label for="lastname">Apellido</label>
+                    <input type="text" class="form-control @error('lastname') border-danger @enderror" id="lastname"
+                        name="lastname" placeholder="Apellidos" value="{{ old('lastname') }}" required>
+                    <span class="invalid-feedback" role="alert">
+                        <strong>Este campo es obligatorio</strong>
+                    </span>
+                    @error('lastname')
+                    <span class="text-danger" role="alert">
+                        <strong>{{ $message }}</strong>
+                    </span>
+                    @enderror
                 </div>
+            </div>
 
-                <div class="form-row mb-2">
-                    <div class="col-sm-12">
-                        <label for="name">Nombre</label>
-                        <input type="text" class="form-control @error('name') border-danger @enderror" id="name" name="name"
-                            placeholder="Nombres" value="{{ old('name') }}" required>
-                        <span class="invalid-feedback" role="alert">
-                            <strong>Este campo es obligatorio</strong>
-                        </span>
-                        @error('name')
-                            <span class="text-danger" role="alert">
-                                <strong>{{ $message }}</strong>
-                            </span>
-                        @enderror
-                    </div>
+            <div class="form-row mb-2">
+                <div class="col-sm-12">
+                    <label for="name">Nombre</label>
+                    <input type="text" class="form-control @error('name') border-danger @enderror" id="name" name="name"
+                        placeholder="Nombres" value="{{ old('name') }}" required>
+                    <span class="invalid-feedback" role="alert">
+                        <strong>Este campo es obligatorio</strong>
+                    </span>
+                    @error('name')
+                    <span class="text-danger" role="alert">
+                        <strong>{{ $message }}</strong>
+                    </span>
+                    @enderror
                 </div>
+            </div>
 
-                <div class="form-row mb-2">
-                    <div class="col-sm-12">
-                        <label for="dni">DNI</label>
-                        <input type="text" class="form-control @error('dni') border-danger @enderror" id="dni" name="dni"
-                            placeholder="DNI" value="{{ old('dni') }}" required>
-                        <span class="invalid-feedback" role="alert">
-                            <strong>Este campo es obligatorio</strong>
-                        </span>
-                        @error('dni')
-                            <span class="text-danger" role="alert">
-                                <strong>{{ $message }}</strong>
-                            </span>
-                        @enderror
-                    </div>
+            <div class="form-row mb-2">
+                <div class="col-sm-12">
+                    <label for="dni">DNI</label>
+                    <input type="text" class="form-control @error('dni') border-danger @enderror" id="dni" name="dni"
+                        placeholder="DNI" value="{{ old('dni') }}" required>
+                    <span class="invalid-feedback" role="alert">
+                        <strong>Este campo es obligatorio</strong>
+                    </span>
+                    @error('dni')
+                    <span class="text-danger" role="alert">
+                        <strong>{{ $message }}</strong>
+                    </span>
+                    @enderror
                 </div>
+            </div>
 
-                <div class="form-row mb-2">
-                    <div class="col-sm-12">
-                        <label for="born_in">Fecha de nacimiento</label>
-                        <input type="date" class="form-control @error('born_in') border-danger @enderror" id="born_in"
-                            name="born_in" placeholder="Fecha de nacimiento" value="{{ old('born_in') }}" required>
-                        <span class="invalid-feedback" role="alert">
-                            <strong>Este campo es obligatorio</strong>
-                        </span>
-                        @error('born_in')
-                            <span class="text-danger" role="alert">
-                                <strong>{{ $message }}</strong>
-                            </span>
-                        @enderror
-                    </div>
+            <div class="form-row mb-2">
+                <div class="col-sm-12">
+                    <label for="born_in">Fecha de nacimiento</label>
+                    <input type="date" class="form-control @error('born_in') border-danger @enderror" id="born_in"
+                        name="born_in" placeholder="Fecha de nacimiento" value="{{ old('born_in') }}" required>
+                    <span class="invalid-feedback" role="alert">
+                        <strong>Este campo es obligatorio</strong>
+                    </span>
+                    @error('born_in')
+                    <span class="text-danger" role="alert">
+                        <strong>{{ $message }}</strong>
+                    </span>
+                    @enderror
                 </div>
+            </div>
 
-                <div class="form-row mb-2">
-                    <div class="col-sm-12">
-                        <label for="photo">Foto <small><i>(OPCIONAL)</i></small></label>
-                        <div class="input-group mb-3">
-                            <div class="custom-file">
-                                <input type="file" name="photo"
-                                    class="custom-file-input @error('photo') border-danger @enderror" id="photo">
-                                <label id="file_inner" class="custom-file-label" for="photo">Elegí un foto para este
-                                    jugador</label>
-                            </div>
+            <div class="form-row mb-2">
+                <div class="col-sm-12">
+                    <label for="photo">Foto <small><i>(OPCIONAL)</i></small></label>
+                    <div class="input-group mb-3">
+                        <div class="custom-file">
+                            <input type="file" name="photo"
+                                class="custom-file-input @error('photo') border-danger @enderror" id="photo">
+                            <label id="file_inner" class="custom-file-label" for="photo">Elegí un foto para este
+                                jugador</label>
                         </div>
-                        @error('photo')
-                            <span class="text-danger" role="alert">
-                                <strong>{{ $message }}</strong>
-                            </span>
-                        @enderror
                     </div>
+                    @error('photo')
+                    <span class="text-danger" role="alert">
+                        <strong>{{ $message }}</strong>
+                    </span>
+                    @enderror
+                </div>
+            </div>
+
+            <div class="form-row mb-2">
+                <div class="col-sm-12 col-md-8">
+                    <label for="club_id">Club</label>
+                    <select class="custom-select" id="clubs" name="club_id" disabled>
+                        <option selected>Elegí el club</option>
+                        @foreach ($clubs as $club)
+                        <option value="{{ $club->id }}">{{ $club->name }}</option>
+                        @endforeach
+                    </select>
+                    <span class="invalid-feedback" role="alert">
+                        <strong>Este campo es obligatorio</strong>
+                    </span>
+                    @error('club_id')
+                    <span class="text-danger" role="alert">
+                        <strong>{{ $message }}</strong>
+                    </span>
+                    @enderror
                 </div>
 
-                <div class="form-row mb-2">
-                    <div class="col-sm-12 col-md-8">
-                        <label for="club_id">Club</label>
-                        <select class="custom-select" id="clubs" name="club_id" disabled>
-                            <option selected>Elegí el club</option>
-                            @foreach ($clubs as $club)
-                                <option value="{{ $club->id }}">{{ $club->name }}</option>
-                            @endforeach
-                        </select>
-                        <span class="invalid-feedback" role="alert">
-                            <strong>Este campo es obligatorio</strong>
-                        </span>
-                        @error('club_id')
-                            <span class="text-danger" role="alert">
-                                <strong>{{ $message }}</strong>
-                            </span>
-                        @enderror
-                    </div>
+                <div class="col-sm-12 col-md-4">
+                    <label for="category_id">Categoría</label>
+                    <select class="custom-select" id="available_categories" name="category_id" disabled>
+                        <option selected>Elegí la categoría</option>
+                    </select>
 
-                    <div class="col-sm-12 col-md-4">
-                        <label for="category_id">Categoría</label>
-                        <select class="custom-select" id="available_categories" name="category_id" disabled>
-                            <option selected>Elegí la categoría</option>
-                        </select>
-
-                        <span class="invalid-feedback" role="alert">
-                            <strong>Este campo es obligatorio</strong>
-                        </span>
-                        @error('category_id')
-                            <span class="text-danger" role="alert">
-                                <strong>{{ $message }}</strong>
-                            </span>
-                        @enderror
-                    </div>
+                    <span class="invalid-feedback" role="alert">
+                        <strong>Este campo es obligatorio</strong>
+                    </span>
+                    @error('category_id')
+                    <span class="text-danger" role="alert">
+                        <strong>{{ $message }}</strong>
+                    </span>
+                    @enderror
                 </div>
+            </div>
 
-                <button class="my-3 btn btn-primary w-100" type="submit">Guardar jugador</button>
-            </form>
-        </div>
+            <button class="my-3 btn btn-primary w-100" type="submit">Guardar jugador</button>
+        </form>
     </div>
+</div>
 
 @endsection
 
 @section('styles')
-    <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
+<link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
 @endsection
 
 @section('scripts')
 
-    <script>
-        const bornDateInput = document.getElementById("born_in");
+<script>
+    const base_url = "{{ env('APP_URL') }}";
+    const bornDateInput = document.getElementById("born_in");
         const clubSelect = document.getElementById("clubs");
         const categorySelect = document.getElementById("available_categories");
 
@@ -181,7 +182,7 @@
 
         const getCategories = () => {
             clubSelect.disabled = false;
-            fetch('http://127.0.0.1:8000/dashboard/players-category', {
+            fetch(`${base_url}/dashboard/players-category`, {
                 method: 'POST',
                 body: JSON.stringify({
                     'born_in': bornDateInput.value,
@@ -206,10 +207,10 @@
 
         clubSelect.addEventListener("change", getCategories);
 
-    </script>
+</script>
 
-    <script>
-        (function() {
+<script>
+    (function() {
             'use strict';
             window.addEventListener('load', function() {
                 // Fetch all the forms we want to apply custom Bootstrap validation styles to
@@ -227,5 +228,5 @@
             }, false);
         })();
 
-    </script>
+</script>
 @endsection
