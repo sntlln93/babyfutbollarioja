6196ebe8c5fc20cb5953ac5a8078603eba725c36
Matias Santillan <sntlln.93@gmail.com>
2021-09-29 10:11:05 -0300

    🔥   Removed unused select2

--- resources/views/dashboard/clubs/edit.blade.php
+++ resources/views/dashboard/clubs/edit.blade.php
@@ -22,19 +22,6 @@
 
 @endsection
 
-@section('styles')
-<link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
-@endsection
-
 @section('scripts')
-<script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
-
-<script>
-    $(document).ready(function() {
-            $('.my-select').select2();
-        });
-
-</script>
-
 @include('dashboard._partials.validation')
 @endsection
--- resources/views/dashboard/tournaments/edit.blade.php
+++ resources/views/dashboard/tournaments/edit.blade.php
@@ -42,15 +42,5 @@
 @endsection
 
 @section('scripts')
-<script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
-
-<script>
-    $(document).ready(function() {
-            $('.my-select').select2();
-        });
-
-</script>
-
 @include('dashboard._partials.validation')
-
 @endsection
